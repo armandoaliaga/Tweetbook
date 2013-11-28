ae1896f8d83fbf61492e1834493fdcdade62604b
armandoaliaga <armandoaliaga92@gmail.com>
2013-11-28 15:33:56 -0400

    arreglando comments

--- app/model/comment.php
+++ app/model/comment.php
@@ -4,7 +4,8 @@ class Comment extends ActiveRecord\Model
 	static $validates_presence_of = array(
 		array('content')); 
          static $belongs_to = array(
-          array('post')
-       );
+          array('post'),
+             array('user')
+       );         
 }
 ?>
