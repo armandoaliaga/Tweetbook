cf0028e695b2432c272cfd3d59b5223f9050236c
Diego R <diroag@gmail.com>
2013-11-29 20:34:54 -0400

    añadidas configs de produccion

--- config/database.php
+++ config/database.php
@@ -1,6 +1,18 @@
 <?php
+//CAMBIAR ENVIRONMENT SEGUN SE NECESITE
+$environment = "development";
+if($environment=="development")
+{
     $GLOBALS['db_name'] = 'tweetbook';
     $GLOBALS['db_host'] = 'localhost';
     $GLOBALS['db_user'] = 'root';
     $GLOBALS['db_password'] = '';
+}
+else if ($environment=="production")
+{
+    $GLOBALS['db_name'] = 'tweetbook';
+    $GLOBALS['db_host'] = '185.28.20.60';
+    $GLOBALS['db_user'] = 'u827889968_tweet';
+    $GLOBALS['db_password'] = 'tweetbook1234';
+}
 ?>
